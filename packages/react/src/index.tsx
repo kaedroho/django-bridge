722b6ab8b0da4d831041b551564f8bd633cd2c5b
Karl Hobley <karl@kaed.uk>
2025-07-21 13:39:10 +0100

    Fix #94: Implement useAutoRefresh hook
    This hook makes it easier to implement auto refresh, which is useful for
    implementing status pages.
    
    For example:
    
    ```tsx
    function MyStatusPage({ status }) {
    
        // If status is pending, auto refresh every 2 seconds
        useAutoRefresh(status === "pending", 2000);
    
        return (
            <h1>The current status is: {status}</h1>
        )
    }
    ```
    
    Auto refreshes happen in the background, if the status changes, the
    component is re-rendered with the new status.

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -220,6 +220,7 @@ export {
 export type { Navigation } from "./contexts";
 export { DirtyFormContext, DirtyFormMarker } from "./dirtyform";
 export type { DirtyForm } from "./dirtyform";
+export { useAutoRefresh } from "./hooks";
 export { type NavigationController } from "./navigation";
 export type { Frame, Message, DjangoBridgeResponse as Response, Metadata };
 export { Link, BuildLinkElement, buildLinkElement };
