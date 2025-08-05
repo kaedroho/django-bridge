0a8dfe1088e0f206cc6c9cd7bf297582f77fb5b2
Karl Hobley <karl@kaed.uk>
2025-08-05 21:36:19 +0100

    Update index.tsx

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
           <span className={styles.visuallyhidden}>Django Bridge</span>
         </Heading>
         <p className={styles.heroSubtitle}>
-          Build apps with <b>Django</b> and{" "}
+          Build <b>Django</b> and{" "}
           <b style={{ whiteSpace: "nowrap" }}>
             <ReactLogo
               style={{
@@ -29,7 +29,7 @@ function HomepageHeader() {
                 paddingTop: "0.1em",
               }}
             />
-            React, the easy way
+            React apps, the easy way
           </b>
         </p>
         <div className={styles.buttons}>
