59a6e41fd494c7783816cffa2f65fb066a6e560a
Kenny Heinonen <kenny.heinonen@fondion.com>
2025-08-25 22:29:53 +0300

    Export more components

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -4,6 +4,15 @@ export { default as App } from "./App.vue";
 // Export Config class - matches React pattern exactly
 export { default as Config } from "./config";
 
+// Export Form component - matches React pattern
+export { default as Form } from "./components/Form.vue";
+
+// Export Link component - matches React pattern
+export { default as Link } from "./components/Link.vue";
+
+// Export RenderFrame component - matches React pattern
+export { default as RenderFrame } from "./components/RenderFrame.vue";
+
 // Export contexts/injection keys
 export {
   NavigationKey,
@@ -12,7 +21,7 @@ export {
   FormSubmissionStatusKey,
   MessagesKey,
 } from "./contexts";
-export type { Navigation } from "./contexts";
+export type { Navigation, Messages } from "./contexts";
 
 // Export dirty form utilities
 export { DirtyFormKey, useDirtyForm, useDirtyFormMarker } from "./dirtyform";
